f795fc5669a8275bbf5d74c0ac54fb7cd22f35ba
jaesu <60861660+hadences@users.noreply.github.com>
2024-09-30 00:25:47 -0500

    Removed unused Mixins

--- src/main/java/net/hadences/mixin/client/SpecterSimpleSpriteProvider.java
+++
@@ -1,9 +0,0 @@
-package net.hadences.mixin.client;
-
-import net.minecraft.client.particle.ParticleManager;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ParticleManager.class)
-public interface SpecterSimpleSpriteProvider {
-
-}
